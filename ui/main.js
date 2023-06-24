46b0f89a03faa1b7b4a0700eeaa29729920cea31
NebulaDev <nebuladev.contact@gmail.com>
2023-06-24 17:28:19 +1200

    Add base audio sampler

---
+++ ui/main.js
@@ -0,0 +1,25 @@
+const resizeConfig = {
+    preserveAspectRatio: false,
+    edges: { left: false, right: false, bottom: false, top: false },
+    margin: 5,
+    restrictSize: { min: { width: 200, height: 250 }, max: { width: 500, height: 500 } }
+}
+
+for (const edge of Object.keys(resizeConfig.edges)) {
+    const className = `.resize-${edge}`
+    interact(className)
+        .resizable({ ...resizeConfig, edges: { [edge]: true } })
+        .on('resizemove', (e) => { resizeMoveListener(e, (edge == 'left' || edge == 'right')) })
+}
+
+function resizeMoveListener(event, widthResize) {
+    var target = event.target;
+
+    if (target.classList.contains('variable-panel')) {
+        var variable = target.getAttribute('data-variable');
+        document.documentElement.style.setProperty(`--${variable}`, (widthResize ? event.rect.width : event.rect.height) + 'px');
+    } else {
+        var whType = widthResize ? 'width' : 'height';
+        target.style[whType] = event.rect[whType] + 'px';
+    }
+}
